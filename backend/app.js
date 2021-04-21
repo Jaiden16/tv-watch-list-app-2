f27838cfb45ba768db43f564d0a58e6afd4c8b7e
jonathan <jonathanfagan@pursuit.org>
2021-04-20 22:22:28 -0400

    added package json

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let cors = require('cors');
-require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
