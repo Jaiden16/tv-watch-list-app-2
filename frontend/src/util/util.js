df7e8f5ab0b004aad71b1857a227426c96f348ab
jonathan <jonathanfagan@pursuit.org>
2021-04-21 23:09:53 -0400

    fixed db

--- frontend/src/util/util.js
+++ frontend/src/util/util.js
@@ -1,6 +1,6 @@
 export const getAPI = () => {
     if (window.location.hostname === 'localhost') {
-        return "http://localhost:3001"
+        return "http://localhost:3100"
     } else {
         return "https://movie-night-jf.herokuapp.com/"
     }
