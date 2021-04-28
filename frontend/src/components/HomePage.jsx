23a26f0e2ab0eb0e3e9a833a323a391c976122bd
jonathan <jonathanfagan@pursuit.org>
2021-04-28 18:19:00 -0400

    homepage re added

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import axios from 'axios' 
+
+export default function Homepage(props) {
+    let { username, avatar } = props
+
+    return (
+        <div className="HomePage">
+            <h1>Welcome {username}</h1>
+            <img src={avatar} alt="broken" style={{ height: 200 }} />
+        </div>
+    )
+}
